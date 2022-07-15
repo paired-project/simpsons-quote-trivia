8db8db7f1b3043ba94d0d424a7d31b57f18a2b92
keonp <keonlmp@gmail.com>
2022-07-15 16:12:50 -0400

    adding JS functionality

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 const app = {};
+app.characterDataArray = [];
+
+app.url = new URL(`https://thesimpsonsquoteapi.glitch.me/quotes`);
+// app.characterName = '';
+// app.characterQuote = '';
+
+// app.url.search = new URLSearchParams({
+//     count: '100'
+// })
+
+app.characterData = () => {
+    fetch(app.url)
+    .then((response) => {
+        console.log('response:', response);
+        return response.json();
+    })
+    .then((jsonData) => {
+        console.log(jsonData);
+        app.getQuote(jsonData);
+    })
+}
+
+
+app.getQuote = (data) => {
+    app.characterName = data[0].character;
+    app.characterQuote = data[0].quote;
+
+    console.log(app.characterName);
+    console.log(app.characterQuote);
+}
+
+
 
 app.init = () => {
-
+    app.characterData();
 }
 
 
