3c8602d00c3d8972ef934b4af35dd79b08c26160
francesm <fr.ncesm@gmail.com>
2022-07-16 09:55:00 -0600

    add character reveal functionality

--- script.js
+++ script.js
@@ -8,10 +8,34 @@ app.getCharactersUrl.search = new URLSearchParams({
     count: '100'
 });
 
+app.landingPageButton = document.querySelector('#landing-page-button');
+app.revealButton = document.querySelector('#reveal-button');
+app.nextQuoteButton = document.querySelector('#next-quote-button');
+
+app.landingPage = document.querySelector('.main-content__text-container--landing-page');
+app.mainPage = document.querySelector('.main-content__text-container--main');
+app.revealPage = document.querySelector('.main-content__text-container--main-reveal');
+
+app.headingElement = app.mainPage.children[0];
+// app.quoteElement = app.mainPage.children[1];
+app.quoteElements = document.querySelectorAll("blockquote");
+app.characterHeading = document.querySelector("#character-name");
+
 app.getCharacterData = (url) => {
+    const buttonText = app.landingPageButton.children[0];
+    const buttonIcon = app.landingPageButton.children[1];
+
+    app.landingPageButton.disabled = true;
+    buttonText.classList.toggle('hide');
+    buttonIcon.classList.toggle('hide');
+
     fetch(url)
         .then((response) => {
-            return response.json();
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error(response.statusText);
+            }
         })
         .then((jsonData) => {
             if (jsonData[1]) {
@@ -19,12 +43,15 @@ app.getCharacterData = (url) => {
             } else {
                 app.getQuote(jsonData);
             }
-        })
+
+            app.landingPageButton.disabled = false;
+            buttonText.classList.toggle('hide');
+            buttonIcon.classList.toggle('hide');
+        });
 }
 
 app.getCharacterList = (data) => {
     console.log(data);
-    // add error handling!!
 }
 
 app.getQuote = (data) => {
@@ -35,27 +62,34 @@ app.getQuote = (data) => {
     console.log(app.characterQuote);
 }
 
-app.landingPageButton = document.querySelector('#landing-page-button');
-app.mainPageButton = document.querySelector('#main-page-button');
+app.updateElements = () => {
+    app.quoteElements.forEach((quoteElement) => {
+        quoteElement.textContent = app.characterQuote;
+    });
 
-app.landingPage = document.querySelector('.main-content__text-container--landing-page');
-app.mainPage = document.querySelector('.main-content__text-container--main');
-
-app.headingElement = app.mainPage.children[0];
-app.quoteElement = app.mainPage.children[1];
-
+    app.characterHeading.textContent = app.characterName;
+}
 
 app.landingPageButton.addEventListener('click', function() {
-    // app.headingElement.textContent = app.characterName;
-    app.quoteElement.textContent = app.characterQuote;
+    app.updateElements();
+    app.getCharacterData(app.getQuoteUrl);
 
     app.landingPage.classList.toggle('inactive');
     app.mainPage.classList.remove('inactive');
-})
+});
 
-// app.mainPageButton.addEventListener('click', function() {
+app.revealButton.addEventListener('click', function() {
+    app.mainPage.classList.toggle('inactive');
+    app.revealPage.classList.toggle('inactive');
+});
 
-// })
+app.nextQuoteButton.addEventListener('click', function() {
+    app.updateElements();
+    app.getCharacterData(app.getQuoteUrl);
+
+    app.revealPage.classList.toggle('inactive');
+    app.mainPage.classList.toggle('inactive');
+});
 
 
 app.init = () => {
