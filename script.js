bf797a64325f67ff5ee6a7a41671e53b642c4ebf
francesm <fr.ncesm@gmail.com>
2022-07-21 17:44:56 -0600

    begin appendQuote logic

--- script.js
+++ script.js
@@ -22,15 +22,6 @@
 
 const app = {};
 
-// URL to retrieve a random quote object
-app.getQuoteUrl = new URL(`https://thesimpsonsquoteapi.glitch.me/quotes`);
-
-// URL to retrieve all available quote objects
-app.getCharactersUrl = new URL(`https://thesimpsonsquoteapi.glitch.me/quotes`);
-app.getCharactersUrl.search = new URLSearchParams({
-    count: '100'
-});
-
 // empty array to hold list of characters
 app.characterList = [];
 
@@ -38,6 +29,7 @@ app.landingPageButton = document.querySelector('#landing-page-button');
 app.revealButton = document.querySelector('#reveal-button');
 app.nextQuoteButton = document.querySelector('#next-quote-button');
 
+app.mainContent = document.querySelector('.main-content');
 app.landingPage = document.querySelector('.main-content__text-container--landing-page');
 app.mainPage = document.querySelector('.main-content__text-container--main');
 app.revealPage = document.querySelector('.main-content__text-container--main-reveal');
@@ -64,103 +56,136 @@ app.getCharacterData = () => {
             }
         })
         .then((jsonData) => {
-            //app.getCharacterList(jsonData);
-            app.getQuotes(jsonData);
             console.log(jsonData);
+            app.getCharacterList(jsonData);
+            app.getQuotes(jsonData);
         });
 }
 
+app.randomizer = (arr) => {
+    return Math.floor(Math.random() * arr.length);
+}
+
 // method which uses the array of all available quotes to store all of the available character names in the app.characterList array
-app.getCharacterList = (data) => {
-    console.log(data);
+app.getCharacterList = (dataArray) => {
+    app.availableCharacters = [];
+
+    dataArray.forEach((characterObject) => {
+        const character = characterObject.character;
+        if (!app.availableCharacters.includes(character)) {
+            app.availableCharacters.push(character);
+        }
+    });
 }
 
 // method which stores the new character name and quotes in namespace variables after receving the API response for a random quote
-app.getQuotes = (data) => {
-    app.characterName = data[0].character;
-    app.characterQuote = data[0].quote;
-    app.characterImage = data[0].image;
+app.getQuotes = (dataArray) => {
+    app.selectedCharacters = [];
 
-
-    console.log(app.characterName);
-    console.log(app.characterQuote);
-    // console.log(app.imageElements);
+    for (let i = 0; i < 10; i++) {
+        const index = app.randomizer(dataArray);
+        app.selectedCharacters.push(dataArray[index]);
+        dataArray.splice(index, 1);
+    }
 }
 
-// method to update the HTML elements using the data from the next random quote object
-app.updateElements = () => {
-    // update the quote elements with the new quote
-    app.quoteElements.forEach((quoteElement) => {
-        quoteElement.textContent = app.characterQuote;
-    });
 
-
-    // update the reveal page heading with the new character name
-    app.characterHeading.textContent = app.characterName;
+//     // update the reveal page heading with the new character name
+//     app.characterHeading.textContent = app.characterName;
     
-    app.imageElements[1].innerHTML = `<div style="background-image: url(${app.characterImage})"></div>`;
+//     app.imageElements[1].innerHTML = `<div style="background-image: url(${app.characterImage})"></div>`;
+// }
+
+// app.updateImages = () => {
+//     app.imageElements.forEach((imageElement) => {
+//         imageElement.classList.toggle('inactive');
+//     })
+// }
+
+app.getNextCharacter = () => {
+    const nextCharacter = app.selectedCharacters[0];
+    app.characterImage = nextCharacter.image;
+    app.characterName = nextCharacter.character;
+    app.characterQuote = nextCharacter.quote;
+
+    app.selectedCharacters.shift();
 }
 
-app.updateImages = () => {
-    app.imageElements.forEach((imageElement) => {
-        imageElement.classList.toggle('inactive');
-    })
+app.getCharacterOptions = () => {
+    const wrongAnswers = [...app.availableCharacters].filter((character) => {
+        return character !== app.characterName;
+    });
+    
+
+    
+    //for (let i = 0; i)
+}
+
+app.appendQuote = () => {
+    app.getNextCharacter();
+    app.getCharacterOptions();
+
+    const nextPage = document.createElement('div');
+    nextPage.className = 'quote';
+    
+    nextPage.innerHTML = `
+    <h2>Who Said...</h2>
+    <blockquote>${app.characterQuote}</blockquote>
+    <div>
+        <button type="button" class="character-option" value=${app.selectedOptions[0]}>${app.selectedOptions[0]}</button>
+        <button type="button" class="character-option"></button>
+        <button type="button" class="character-option"></button>
+        <button type="button" class="character-option"></button>
+    </div>
+    `;
 }
 
 // when the landing page button is clicked...
 app.landingPageButton.addEventListener('click', function() {
-    // update the HTML elements with the values store in namespace variables from the previous api call
-    app.updateElements();
-    // load the next quote
-    app.getCharacterData(app.getQuoteUrl);
-
-    // hide the landing page and display the main page
-    app.mainPage.classList.toggle('inactive');
-    app.landingPage.classList.toggle('inactive'); 
+    app.appendQuote();
 });
 
-// when the reveal button is clicked...
-app.revealButton.addEventListener('click', function() {
-    // hide the main page and display the reveal page
-    app.mainPage.classList.toggle('inactive');
-    app.revealPage.classList.toggle('inactive');
+// // when the reveal button is clicked...
+// app.revealButton.addEventListener('click', function() {
+//     // hide the main page and display the reveal page
+//     app.mainPage.classList.toggle('inactive');
+//     app.revealPage.classList.toggle('inactive');
 
-    app.updateImages();
-});
+//     app.updateImages();
+// });
 
-// when the next quote button is clicked...
-app.nextQuoteButton.addEventListener('click', function() {
-    // display the loading page
-    app.loadingPage.classList.toggle('inactive');    
+// // when the next quote button is clicked...
+// app.nextQuoteButton.addEventListener('click', function() {
+//     // display the loading page
+//     app.loadingPage.classList.toggle('inactive');    
     
-    // hide the reveal page
-    app.revealPage.classList.toggle('inactive');
+//     // hide the reveal page
+//     app.revealPage.classList.toggle('inactive');
     
-    // after 2s...
-    setTimeout(() => {
-        // update the HTML elements with the values store in namespace variables from the previous api call
-        app.updateElements();
+//     // after 2s...
+//     setTimeout(() => {
+//         // update the HTML elements with the values store in namespace variables from the previous api call
+//         app.updateElements();
         
-        // load the next quote
-        app.getCharacterData(app.getQuoteUrl);
+//         // load the next quote
+//         app.getCharacterData(app.getQuoteUrl);
 
-        // hide the loading page
-        app.loadingPage.classList.toggle('inactive');
-    }, 2000)
+//         // hide the loading page
+//         app.loadingPage.classList.toggle('inactive');
+//     }, 2000)
     
     
-    // display the main page
-    app.mainPage.classList.toggle('inactive');
+//     // display the main page
+//     app.mainPage.classList.toggle('inactive');
 
-    app.updateImages();
-});
+//     app.updateImages();
+// });
 
 // on page load...
 app.init = () => {
     // create an array of available character names
     // load the quotes
     //app.getCharacterList();
-
     app.getCharacterData();
 }
 
